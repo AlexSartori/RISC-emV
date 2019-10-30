c5176e91218f156a28a18cfd04feb9ada07d4fd9
Alex Sartori <alex.sartori1997@gmail.com>
2019-10-30 18:46:58 +0100

    Add tests for ProgramLoader

---
+++ sample_programs/_test1.s
@@ -0,0 +1,4 @@
+add r3, r2, r1
+or r4, r5, r6
+
+sub r2, r3, r4
---
+++ tests/test_ProgramLoader.py
@@ -0,0 +1,24 @@
+import os
+from riscemv.ProgramLoader import ProgramLoader
+
+
+
+def test_asm_to_binary():
+    PL = ProgramLoader(32)
+    inst = "add r3, r0, r1"
+    exp = "00000000000100000000000110110011"
+
+    assert PL.asm_to_binary(inst) == exp
+
+
+def test_load_assembly_code():
+    PL = ProgramLoader(32)
+    f = os.path.join(os.path.dirname(__file__), "..", "sample_programs", "_test1.s")
+    exp = [
+        '00000000000100010000000110110011',
+        '00000000011000101110001000110011',
+        '00000000010000011000000100110011'
+    ]
+    PL.load_assembly_code(open(f).read())
+
+    assert PL.lines == exp
