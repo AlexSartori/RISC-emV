1389167ec1a13c7167ba5e73067f84ff2f185bba
Alex Sartori <alex.sartori1997@gmail.com>
2019-10-31 17:43:15 +0100

    Add RegisterView to GUI

--- riscemv/gui/MainWindow.py
+++ riscemv/gui/MainWindow.py
@@ -1,24 +1,59 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 
-class MainWindow(QtWidgets.QWidget):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        # QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle("RISC-emV")
+        self.setMinimumSize(600, 300)
+
         self.initUI()
+        self.statusBar().showMessage("Ready")
 
 
     def initUI(self):
-        self.setWindowTitle("RISC-emV")
+        self.central_split = QtWidgets.QSplitter()
+        self.setCentralWidget(self.central_split)
 
         self.welcome = QtWidgets.QLabel()
         self.welcome.setText("Welcome. Work is in progress.")
         self.welcome.setFont(QtGui.QFont('SansSerif', 20))
+
+        self.register_view = self.create_register_view()
+        self.central_split.addWidget(self.register_view)
+        self.central_split.addWidget(self.welcome)
         # self.welcome.setAlignment(QtCore.Qt.AlignHCenter)
         # self.welcome.adjustSize()
 
-        self.main_hbox = QtWidgets.QHBoxLayout()
-        self.main_hbox.addStretch(1)
-        self.main_hbox.addWidget(self.welcome)
-        self.main_hbox.addStretch(1)
 
-        self.setLayout(self.main_hbox)
+
+    def create_register_view(self):
+        frame = QtWidgets.QGroupBox()
+        frame.setTitle("Registers")
+        frame.setLayout(QtWidgets.QVBoxLayout())
+
+        format_chooser = QtWidgets.QFrame()
+        format_chooser.setLayout(QtWidgets.QHBoxLayout())
+
+        fmt_binary = QtWidgets.QPushButton()
+        fmt_binary.setText("BIN")
+        fmt_decimal = QtWidgets.QPushButton()
+        fmt_decimal.setText("DEC")
+        fmt_hex = QtWidgets.QPushButton()
+        fmt_hex.setText("HEX")
+
+        format_chooser.layout().addWidget(fmt_binary)
+        format_chooser.layout().addWidget(fmt_decimal)
+        format_chooser.layout().addWidget(fmt_hex)
+
+        rf_table = QtWidgets.QTableWidget()
+        rf_table.setColumnCount(2)
+        rf_table.setRowCount(32)
+
+        for r in range(32):
+            rf_table.setItem(r, 0, QtWidgets.QTableWidgetItem("R" + str(r)))
+            rf_table.setItem(r, 1, QtWidgets.QTableWidgetItem("0101001010"))
+
+        frame.layout().addWidget(format_chooser)
+        frame.layout().addWidget(rf_table)
+
+        return frame
