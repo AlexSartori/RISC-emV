fa1afec449b1383b54ddf5289fedfda155843da1
Alex Sartori <alex.sartori1997@gmail.com>
2019-11-08 13:23:52 +0100

    [GUI] Connect toolbar action to open files

--- riscemv/gui/CodeTextBox.py
+++ riscemv/gui/CodeTextBox.py
@@ -10,9 +10,13 @@ class CodeTextBox(QtWidgets.QFrame):
         title.setText("Code Viewer:")
         self.layout().addWidget(title)
 
-        text_edit = QtWidgets.QTextEdit()
-        text_edit.setLineWrapMode(QtWidgets.QTextEdit.NoWrap)
-        text_edit.setFont(QtGui.QFont('Monospace', 14))
-        text_edit.setText("ADD r4, r3, r5 ; RV32I addition\nSUB r5, r4 r12\nbla bla")
+        self.text_edit = QtWidgets.QTextEdit()
+        self.text_edit.setLineWrapMode(QtWidgets.QTextEdit.NoWrap)
+        self.text_edit.setFont(QtGui.QFont('Monospace', 14))
+        self.text_edit.setText("ADD r4, r3, r5 ; RV32I addition\nSUB r5, r4 r12\nbla bla")
 
-        self.layout().addWidget(text_edit)
+        self.layout().addWidget(self.text_edit)
+
+
+    def setText(self, txt):
+        self.text_edit.setText(txt)
--- riscemv/gui/MainWindow.py
+++ riscemv/gui/MainWindow.py
@@ -11,8 +11,20 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle("RISC-emV")
+        self.setMinimumSize(self.sizeHint())
 
-        self.toolbar = self.create_toolbar()
+        openAction  = QtWidgets.QAction(QtGui.QIcon.fromTheme('document-open'), 'Open', self)
+        startAction = QtWidgets.QAction(QtGui.QIcon.fromTheme('media-playback-start'), 'Start', self)
+        stepAction  = QtWidgets.QAction(QtGui.QIcon.fromTheme('go-next'), 'Step Forward', self)
+        # startAction.setShortcut('Ctrl+R')
+        # startAction.triggered.connect(qApp.quit)
+
+        openAction.triggered.connect(self.openDocument)
+
+        self.toolbar = self.addToolBar('HomeToolbar')
+        self.toolbar.addAction(openAction)
+        self.toolbar.addAction(startAction)
+        self.toolbar.addAction(stepAction)
 
         self.initUI()
         self.statusBar().showMessage("Ready")
@@ -22,57 +34,39 @@ class MainWindow(QtWidgets.QMainWindow):
         self.central_split = QtWidgets.QSplitter()
         self.setCentralWidget(self.central_split)
 
-        # Left pane (code view)
-        self.text_edit = CodeTextBox()
-        self.instbuffer_view = InstBufferViewer()
-        self.code_pane = self.create_code_pane()
 
-        # Right pane (registers, etc.)
+        self.code_textbox = CodeTextBox()
+        self.instbuffer_view = InstBufferViewer()
+
+        self.code_pane = QtWidgets.QSplitter()
+        self.code_pane.setOrientation(QtCore.Qt.Vertical)
+        self.code_pane.setMinimumWidth(250)
+        self.code_pane.addWidget(self.code_textbox)
+        self.code_pane.addWidget(self.instbuffer_view)
+        self.code_pane.setStretchFactor(0, 3)
+        self.code_pane.setStretchFactor(1, 1)
+
+
         self.register_view = RegisterViewer()
         self.regstatus_view = RegStatusViewer()
         self.resstations_view = ResStationsViewer()
-        self.status_pane = self.create_status_pane()
 
-        # All together
+        self.status_pane = QtWidgets.QFrame()
+        self.status_pane.setLayout(QtWidgets.QVBoxLayout())
+        self.status_pane.layout().addWidget(self.register_view)
+        self.status_pane.layout().addWidget(self.regstatus_view)
+        self.status_pane.layout().addWidget(self.resstations_view)
+        self.status_pane.layout().addStretch(1)
+
+
         self.central_split.addWidget(self.code_pane)
         self.central_split.setStretchFactor(0, 1)
         self.central_split.addWidget(self.status_pane)
         self.central_split.setStretchFactor(1, 3)
 
 
-    def create_toolbar(self):
-        openAction  = QtWidgets.QAction(QtGui.QIcon.fromTheme('document-open'), 'Open', self)
-        startAction = QtWidgets.QAction(QtGui.QIcon.fromTheme('media-playback-start'), 'Start', self)
-        stepAction  = QtWidgets.QAction(QtGui.QIcon.fromTheme('go-next'), 'Step Forward', self)
-        # startAction.setShortcut('Ctrl+R')
-        # startAction.triggered.connect(qApp.quit)
-
-        toolbar = self.addToolBar('HomeToolbar')
-        toolbar.addAction(openAction)
-        toolbar.addAction(startAction)
-        toolbar.addAction(stepAction)
-
-        return toolbar
-
-
-    def create_code_pane(self):
-        code_pane = QtWidgets.QSplitter()
-        code_pane.setOrientation(QtCore.Qt.Vertical)
-        code_pane.setMinimumWidth(250)
-        code_pane.addWidget(self.text_edit)
-        code_pane.addWidget(self.instbuffer_view)
-        code_pane.setStretchFactor(0, 3)
-        code_pane.setStretchFactor(1, 1)
-
-        return code_pane
-
-
-    def create_status_pane(self):
-        status_pane = QtWidgets.QFrame()
-        status_pane.setLayout(QtWidgets.QVBoxLayout())
-        status_pane.layout().addWidget(self.register_view)
-        status_pane.layout().addWidget(self.regstatus_view)
-        status_pane.layout().addWidget(self.resstations_view)
-        status_pane.layout().addStretch(1)
-
-        return status_pane
+    def openDocument(self):
+        # options = QtWidgets.QFileDialog.Options()
+        # options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open file", "", "All Files (*)") # TODO: only .s or ELF
+        self.code_textbox.setText(open(filename).read())
