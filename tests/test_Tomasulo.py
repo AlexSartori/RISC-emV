c782b2c865f0ec7f5151768843d16f8cc9cacf02
Davide <davide.zanella10@gmail.com>
2019-12-10 15:04:25 +0100

    Fix Tomasulo tests

--- tests/test_Tomasulo.py
+++ tests/test_Tomasulo.py
@@ -6,8 +6,9 @@ def test_tomasulo():
     xlen = 32
     n_adders = n_mult = n_div = n_ld = 1
     code_text = "addi x10, x9, 12"
+    thread_id = 0
 
-    tomasulo = Tomasulo(xlen, n_adders, n_mult, n_div, n_ld, n_adders, n_mult, n_div, n_ld)
+    tomasulo = Tomasulo(xlen, thread_id, n_adders, n_mult, n_div, n_ld, n_adders, n_mult, n_div, n_ld)
     P = Program(tomasulo.DM)
     P.load_text(code_text)
 
