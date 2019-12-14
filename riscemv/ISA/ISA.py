5398c990049a2d4dd973906f7627b21152d63359
Alex Sartori <alex.sartori1997@gmail.com>
2019-12-14 12:09:31 +0100

    Add memory dump to ELF.py debug log

--- riscemv/ISA/ISA.py
+++ riscemv/ISA/ISA.py
@@ -203,7 +203,7 @@ class ISA:
 
     def instruction_from_bin(self, binary_code, pc):
         opcode = binary_code[25:32]
-        # print("OPCODE:", opcode)
+        print("OPCODE:", opcode)
 
         for instr_type in self.ISA:
             for instr_code in self.ISA[instr_type]:
@@ -323,3 +323,5 @@ class ISA:
                             return inst
                     else:
                         raise NotImplementedError()
+                else:
+                    raise NotImplementedError("Unknown OPCODE")
