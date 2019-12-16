03ebbf0c3e33b59202e9456765b419fdc214637a
Alex Sartori <alex.sartori1997@gmail.com>
2019-12-16 10:51:02 +0100

    Add tests for Program.py

--- tests/test_Program.py
+++ tests/test_Program.py
@@ -1,24 +1,35 @@
 import os
 from riscemv.Program import Program
+from riscemv.DataMemory import DataMemory
 
 
+def test_load_text():
+    image = '18 02 49 01 90 02 89 01 00 94 86 63 00 95 24 23 42 92 c1 01 00 a4 86 33'
+    image = image.split(' ')
 
-# def test_asm_to_binary():
-#     PL = ProgramLoader(32)
-#     inst = "add r3, r0, r1"
-#     exp = "00000000000100000000000110110011"
-#
-#     assert PL.asm_to_binary(inst) == exp
+    DM = DataMemory(128)
+    p = Program(DM)
+
+    p.load_text(open("sample_programs/_beq.s", 'r').read())
+
+    for i, v in enumerate((DM)):
+        if i == len(image):
+            break
+        else:
+            assert '{:02x}'.format(v) == image[i], "Data memory differs at byte " + str(i)
 
 
-# def test_load_assembly_code():
-#     PL = ProgramLoader(32)
-#     f = os.path.join(os.path.dirname(__file__), "..", "sample_programs", "_test1.s")
-#     exp = [
-#         '00000000000100010000000110110011',
-#         '00000000011000101110001000110011',
-#         '01000000010000011000000100110011'
-#     ]
-#     PL.load_assembly_code(open(f).read())
-#
-#     assert PL.lines == exp
+def test_load_elf():
+    image = '13 01 01 fe 23 2e 81 00 13 04 01 02 93 07 a0 00 23 26 f4 fe 93 07 c0 00 23 24 f4 fe 03 27 c4 fe 83 27 84 fe b3 07 f7 00 23 22 f4 fe 93 07 00 00 13 85 07 00 03 24 c1 01 13 01 01 02 67 80'
+    image = image.split(' ')
+
+    DM = DataMemory(128)
+    p = Program(DM)
+
+    p.load_machine_code("sample_programs/test_32.o")
+
+    for i, v in enumerate((DM)):
+        if i == len(image):
+            break
+        else:
+            assert '{:02x}'.format(v) == image[i], "Data memory differs at byte " + str(i)
